c3e48b8115961e3d3abd08c08170b118d1ce7489
manuel <mduvens@gmail.com>
2021-07-18 19:20:31 +0100

    yo

---
+++ precache-manifest.04e7d204401e2423938c44445b0816ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e2b47d0d9dc009838f",
+    "url": "/css/about.1633070d.css"
+  },
+  {
+    "revision": "120a2de026783024e28d",
+    "url": "/css/app.96f2fb8f.css"
+  },
+  {
+    "revision": "66e1a25a5350686405593220a07c665a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21e2b47d0d9dc009838f",
+    "url": "/js/about.d28d0ac7.js"
+  },
+  {
+    "revision": "120a2de026783024e28d",
+    "url": "/js/app.f8defbfc.js"
+  },
+  {
+    "revision": "a63689c1a28b0991075b",
+    "url": "/js/chunk-vendors.60e86d18.js"
+  },
+  {
+    "revision": "442c94ebe800f3161ff9cfaf9add627d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
