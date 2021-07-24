0c12b0fb52673697fdec6b8894b3d236c23c914d
manuel <mduvens@gmail.com>
2021-07-24 14:08:46 +0100

    point to project https 2

---
+++ precache-manifest.c6152b86bb398bb78996c89d8d64223d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c7885e1ae94cb862f1",
+    "url": "/css/about.1633070d.css"
+  },
+  {
+    "revision": "218461a843b5f8b1d811",
+    "url": "/css/app.96f2fb8f.css"
+  },
+  {
+    "revision": "885f28f63a28c0d59fa3d46a9dc9a918",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9c7885e1ae94cb862f1",
+    "url": "/js/about.5821dae4.js"
+  },
+  {
+    "revision": "218461a843b5f8b1d811",
+    "url": "/js/app.1c3d9558.js"
+  },
+  {
+    "revision": "46c2b8f2148cfaa2ab44",
+    "url": "/js/catchAll.f99f56f5.js"
+  },
+  {
+    "revision": "778c9637737b5346db72",
+    "url": "/js/chunk-vendors.390fe004.js"
+  },
+  {
+    "revision": "442c94ebe800f3161ff9cfaf9add627d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
